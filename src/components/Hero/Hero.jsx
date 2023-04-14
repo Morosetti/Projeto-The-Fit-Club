cbc0b4077db9ee76fef8b3f92458a0036c048ea1
Morosetti <morosetti1@gmail.com>
2023-04-14 13:40:32 -0300

    primeiro commit

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Hero.css'
+
+function Hero() {
+  return (
+    <div className="hero">
+        <div className="esquerda-h"></div>
+        <div className="direita-h"></div>
+    </div>
+  )
+}
+
+export default Hero
