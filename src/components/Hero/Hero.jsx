36999df073ffaf0d8038cbf6fca0e642295b9915
Morosetti <morosetti1@gmail.com>
2023-05-03 01:02:43 -0300

    adições de buttons ao header

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -12,13 +12,35 @@ const Hero = () => {
               <div></div>
               <span>O melhor club fitness da cidade</span>
             </div>
-
             <div className="hero-text">
               <div>
-              <span>Conquiste </span>
-              <span>seu</span>
+                <span className='stroke-text'>Conquiste </span>
+                <span>seu</span>
               </div>
-              <div><span>corpo ideal</span></div>
+              <div>
+                <span>corpo ideal</span>
+              </div>
+              <div>
+                <span>Aqui está a sua ajuda para chegar no seu corpo ideal, ter saúde e viver sua vida ao maximo.</span>
+              </div>
+            </div>
+            <div className="figuras">
+              <div>
+                <span>+140</span>
+                <span>Treinadores licenciados</span>
+              </div>
+              <div>
+                <span>+978</span>
+                <span>Membros cadastrados</span>
+              </div>
+              <div>
+                <span>+50</span>
+                <span>Treinos personalizados</span>
+              </div>
+            </div>
+            <div className="hero-buttons">
+              <buttons className="btn">Comece já</buttons>
+              <buttons className="btn">Saiba mais</buttons>
             </div>
         </div>
         <div className="direita-h">dir</div>
