23774d42284cc079202e6799f673730944c3384d
Morosetti <morosetti1@gmail.com>
2023-06-28 22:03:00 -0300

    Adição do footer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Depoimentos from './components/Depoimentos/Depoimentos';
+import Footer from './components/Footer/Footer';
 import Hero from './components/Hero/Hero';
 import Join from './components/Join/Join';
 import Motivos from './components/Motivos/Motivos';
@@ -15,6 +16,7 @@ function App() {
       <Planos/>
       <Depoimentos/>
       <Join/>
+      <Footer/>
     </div>
   );
 }
